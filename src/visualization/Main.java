26819379077910f0f98802e8df0640fd5c46ae5a
Verica Lazova <vericalcroft@gmail.com>
2014-07-22 17:47:51 +0200

    Visualization finished

--- src/visualization/Main.java
+++ src/visualization/Main.java
@@ -3,18 +3,16 @@ package visualization;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.graphstream.graph.Graph;
-import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.file.FileSinkImages;
 import org.graphstream.stream.file.FileSinkImages.LayoutPolicy;
-import org.graphstream.stream.file.FileSinkImages.OutputPolicy;
 import org.graphstream.stream.file.FileSinkImages.OutputType;
 import org.graphstream.stream.file.FileSinkImages.Quality;
 import org.graphstream.stream.file.FileSinkImages.RendererType;
-import org.graphstream.stream.file.FileSinkImages.Resolution;
 import org.graphstream.stream.file.FileSinkImages.Resolutions;
 
 import ranking.LinkWeighter;
@@ -29,54 +27,44 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		MatchUpGraph g = new MatchUpGraph();                          // new graph object
+		// Build the graph
+		MatchUpGraph g = new MatchUpGraph();                          
 		try {
 			g.readDataIn();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}		
-		g.buildGraph(LinkWeighter.LOSS_RATIO);                        // build the graph
-		Matrix ranks = PageRanker.pagerank(g.getAdj(), 0.2);          // calculate pagerank
+		g.buildGraph(LinkWeighter.LOSS_RATIO); 
 		
+		// calculate pagerank, and sort the teams with respect to their ranks
+		Matrix ranks = PageRanker.pagerank(g.getAdj(), 0.2);          
 		ArrayList<PagerankPair> pRank = new ArrayList<PagerankPair>();
 		for (int i = 0; i < ranks.getRowDimension(); i++)
 			pRank.add(new PagerankPair(i, ranks.get(i, 0)));
 		Collections.sort(pRank);
 		
-		
+		// calculate the size of each node, which depends on it's pagerank
 		int [] nodeSize = new int [pRank.size()];
-		nodeSize[pRank.get(0).id] = 18;
+		nodeSize[pRank.get(0).id] = 13;
 		for (int i = 1; i < ranks.getRowDimension(); i++) 
 			nodeSize[pRank.get(i).id] = (int) Math.round(nodeSize[pRank.get(i-1).id] * 
 					(pRank.get(i).rank / pRank.get(i-1).rank));
 		
-			
+		// add nodes to the graph	
 		Matrix A = g.getAdj();		
-		Graph graph = new SingleGraph("football-matches", true, true);  // graph with only one edge between vertices
+		Graph graph = new SingleGraph("football");
 		for (int i = 0; i < A.getRowDimension(); i++) { 
 			graph.addNode(g.id[i] + "");
 			graph.getNode(g.id[i] + "").setAttribute(
-					"ui.style", "size: " + nodeSize[i] + "px; fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + g.id[i] + ".gif');");
-			//graph.getNode(g.id[i] + "").setAttribute("ui.style", 
-				//	"fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + g.id[i] + ".gif');");			
+					"ui.style", "size: " + nodeSize[i] + "px; fill-image: url('/home/skyra/workspace/football_ranks/files/flags-svg/" + g.id[i] + ".png');");
 		}
-		graph.getNode(372 + "").setAttribute("ui.style", 
-				"fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + 372 + ".png');");
-		graph.getNode(263 + "").setAttribute("ui.style", 
-				"fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + 263 + ".png');");
-		graph.getNode(345 + "").setAttribute("ui.style", 
-				"fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + 345 + ".png');");
-		graph.getNode(307 + "").setAttribute("ui.style", 
-				"fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + 307 + ".png');");
-		graph.getNode(383 + "").setAttribute("ui.style", 
-				"fill-image: url('/home/skyra/workspace/football_ranks/files/flags/" + 383 + ".png');");
-	
-		graph.setAttribute("layout.stabilization-limit", 0.1);
 			
+		// remove some links, and add the rest to the graph
+		A = leaveTopLinks(A, 5);
 		int from, to;
 		for (int i = 0; i < A.getRowDimension(); i++) {
 			for (int j = 0; j < A.getColumnDimension(); j++) {
-				if (A.get(i, j) == 0)
+				if (A.get(i, j) <= 0)
 					continue;
 				from = g.id[i];
 				to = g.id[j];
@@ -84,29 +72,67 @@ public class Main {
 			}
 		}
 		
+		// set some atributes about the graphics
 		graph.addAttribute("ui.antialias", true);
-		graph.addAttribute("ui.quality");
+		graph.addAttribute("ui.quality", true);
 		graph.addAttribute("ui.stylesheet", "url('files/css/graph-style.css')");
 		
+		// set the sink
+		FileSinkImages pic = new FileSinkImages(OutputType.PNG, Resolutions.QSXGA);
+		pic.setStyleSheet("url('files/css/graph-style.css')");
+		pic.setLayoutPolicy(LayoutPolicy.COMPUTED_FULLY_AT_NEW_IMAGE);
+		pic.setQuality(Quality.HIGH);
+		pic.setRenderer(RendererType.SCALA);
+		
+		// set the renderer
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 		
-		FileSinkImages pic = new FileSinkImages(OutputType.PNG,
-				new FileSinkImages.CustomResolution(3000, 3000));
-		pic.setLayoutPolicy(LayoutPolicy.COMPUTED_IN_LAYOUT_RUNNER);
-		pic.setQuality(Quality.HIGH);
-		pic.setAutofit(true);
-		pic.setRenderer(RendererType.SCALA);
-		pic.setLayoutStabilizationLimit(0.1);
-		pic.stabilizeLayout(0.1);
-		
+		// create an image
 		try {
-			pic.writeAll(graph, "files/images/graph.png");
+			pic.writeAll(graph, "files/images/graph.png");			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 					
-		graph.display();
+		// display graph
+		graph.display().enableAutoLayout();	
+		
+		// take screenshots every 2 sec.
+		while (true) {
+			graph.addAttribute("ui.screenshot", "files/images/graph-screen.bmp");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
+	
+	
+	/**
+	 * Remove all but the top N links
+	 * 
+	 * @param m
+	 * @param topN
+	 * @return
+	 */
+	public static Matrix leaveTopLinks (Matrix m, int topN) {
+		Matrix tmp;
+		double [] elements;
+		double min;
+		for (int i = 0; i < m.getRowDimension(); i++) {
+			tmp = m.getMatrix(i, i, 0, m.getColumnDimension()-1);
+			elements = tmp.getRowPackedCopy();
+			Arrays.sort(elements);
+			min = elements[elements.length-topN];
+			
+			for (int j = 0; j < m.getColumnDimension(); j++) {
+				if (m.get(i, j) < min)
+					m.set(i, j, 0);
+			}					
+		}
+		
+		return m;
+	}
+	
 }
